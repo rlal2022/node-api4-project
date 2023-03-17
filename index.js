cfce62f48b85cd97e15410ec871d2f993e60a13e
user name <you@example.com>
2023-03-16 17:13:28 -0700

    done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const server = express();
+const cors = require("cors");
 
 server.use(express.json());
 
@@ -12,6 +14,12 @@ server.use("*", (req, res) => {
 });
 
 const port = process.env.PORT || 9000;
+
+server.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message, customMessage: "We ran into an error!" });
+});
 
 server.listen(9000, () => {
   console.log(`Server Running on http://localhost:${port}`);
